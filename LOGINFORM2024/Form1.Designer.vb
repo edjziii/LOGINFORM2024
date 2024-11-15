8620958e9619493a8be4c5f0a2d61aa4b309fe56
REF2024 <REF2024@PL42>
2024-11-15 11:57:47 +0800

    Add project files.

---
+++ LOGINFORM2024/Form1.Designer.vb
@@ -0,0 +1,83 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        btnTest = New Button()
+        TextBox_UN = New TextBox()
+        TextBox_Pass = New TextBox()
+        btn_LogIn = New Button()
+        SuspendLayout()
+        ' 
+        ' btnTest
+        ' 
+        btnTest.Location = New Point(23, 25)
+        btnTest.Name = "btnTest"
+        btnTest.Size = New Size(154, 68)
+        btnTest.TabIndex = 0
+        btnTest.Text = "Check Connection"
+        btnTest.UseVisualStyleBackColor = True
+        ' 
+        ' TextBox_UN
+        ' 
+        TextBox_UN.Location = New Point(267, 144)
+        TextBox_UN.Name = "TextBox_UN"
+        TextBox_UN.Size = New Size(100, 23)
+        TextBox_UN.TabIndex = 1
+        ' 
+        ' TextBox_Pass
+        ' 
+        TextBox_Pass.Location = New Point(267, 182)
+        TextBox_Pass.Name = "TextBox_Pass"
+        TextBox_Pass.Size = New Size(100, 23)
+        TextBox_Pass.TabIndex = 2
+        ' 
+        ' btn_LogIn
+        ' 
+        btn_LogIn.Location = New Point(267, 225)
+        btn_LogIn.Name = "btn_LogIn"
+        btn_LogIn.Size = New Size(100, 34)
+        btn_LogIn.TabIndex = 3
+        btn_LogIn.Text = "Log In "
+        btn_LogIn.UseVisualStyleBackColor = True
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(800, 450)
+        Controls.Add(btn_LogIn)
+        Controls.Add(TextBox_Pass)
+        Controls.Add(TextBox_UN)
+        Controls.Add(btnTest)
+        Name = "Form1"
+        Text = "LOG IN FORM"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents btnTest As Button
+    Friend WithEvents TextBox_UN As TextBox
+    Friend WithEvents TextBox_Pass As TextBox
+    Friend WithEvents btn_LogIn As Button
+
+End Class
---
+++ LOGINFORM2024/Form1.vb
@@ -0,0 +1,52 @@
+﻿Imports MySql.Data.MySqlClient
+
+Public Class Form1
+    Dim conn As MySqlConnection
+    Dim COMMAND As MySqlCommand
+
+    ' Test MySQL Connection (not necessary for login, but useful for debugging)
+    Private Sub btnTest_Click(sender As Object, e As EventArgs) Handles btnTest.Click
+        conn = New MySqlConnection
+        conn.ConnectionString = "server=127.0.0.1;userid=root;password='';database=login"
+
+        Try
+            conn.Open()
+            MessageBox.Show("Connection to MySQL test database was successful!!!!", "TESTING CONNECTION TO MySQL DATABASE")
+            conn.Close()
+        Catch ex As Exception
+            MsgBox(ex.Message)
+            conn.Close()
+        End Try
+    End Sub
+
+    ' Log In Button Click Event
+    Private Sub btn_LogIn_Click(sender As Object, e As EventArgs) Handles btn_LogIn.Click
+        conn = New MySqlConnection
+        conn.ConnectionString = "server=127.0.0.1;userid=root;password='';database=login"
+        Dim READER As MySqlDataReader
+        Try
+            conn.Open()
+            Dim Query As String
+            Query = "SELECT * FROM Account WHERE username = '" & TextBox_UN.Text & "' AND password = '" & TextBox_Pass.Text & "'"
+            COMMAND = New MySqlCommand(Query, conn)
+            READER = COMMAND.ExecuteReader
+            Dim count As Integer
+            count = 0
+            While READER.Read
+                count = count + 1
+            End While
+            If count = 1 Then
+                MessageBox.Show("Login successful!")
+            ElseIf count > 1 Then
+                MessageBox.Show("Username and password are duplicate!")
+            Else
+                MessageBox.Show("Username and password are incorrect!")
+            End If
+
+            conn.Close()
+        Catch ex As Exception
+            MsgBox(ex.Message)
+            conn.Close()
+        End Try
+    End Sub
+End Class
